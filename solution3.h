6acbd9b765b7f017f7a31eb591cb9efcaca99b17
Chris Dewey <chris.dewey310@gmail.com>
2020-01-20 15:46:19 +0000

    Completed solution 3. My original issue was trying to use brute force. Changing my method to only check for values after dividing the original number by its found factors instead of all numbers in between, which takes a considerably longer ammount of time.

--- solution3.h
+++ solution3.h
@@ -4,35 +4,30 @@
 //The prime factors of 13195 are 5, 7, 13 and 29.
 //What is the largest prime factor of the number 600851475143 ?
 
-bool isNotPrime(long input)
+
+void three(long long input)
 {
-	bool notPrime = false;
-	for (int i = 2; i <= input / 2; ++i)
+	long long number = input;
+	long biggestFactorial = 0;
+	int counter = 2;
+
+	while (counter * counter <= number)
 	{
-		if (input % i == 0)
+		if (number % counter == 0)
 		{
-			notPrime = true;
-			break;
+			number = number / counter;
+			biggestFactorial = counter;
+		}
+		else
+		{
+			counter++;
 		}
 	}
-	if (notPrime == false) std::cout << "This number is already prime." << std::endl;
-	return(notPrime);
-}
-
-void three(long input)
-{
-	if (isNotPrime(input) == true)
+	if (number > biggestFactorial)
 	{
-		int largestPrime = 0;
-		for (int i = 2; i <= input / 2; ++i)
-		{
-			if (input % i == 0)
-			{
-				largestPrime = i;
-			}
-		};
-		std::cout << "The largest prime factor of " << input
-			<< " is: " << largestPrime << std::endl;
+		biggestFactorial = number;
 	}
 
+	std::cout << "The largest prime factor of " << input
+		<< " is: " << biggestFactorial << std::endl;
 }
